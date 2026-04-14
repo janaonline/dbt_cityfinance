df74119708889e3dd8b1ea991c4c81882b584440
Sachin Kumar <sachinkumarsn2@gmail.com>
2026-04-14 16:12:36 +0530

    Refactor AFS digitisation models: remove afs_summary.sql and add afs_xl_files_report.sql for improved query performance

--- models/afs_digitisation/marts/afs_xl_files_report.sql
+++ models/afs_digitisation/marts/afs_xl_files_report.sql
@@ -2,14 +2,36 @@
 -- Tagged with 'ap_api_poc' for easy identification and selective execution
 {{ config(materialized='table', tags=['afs_digitisation']) }}
 
-
-afsxlfiles AS (
+WITH
+afs AS (
     SELECT *
     FROM {{ source('afs_digitisation', 'afs_xl_files') }}
 ),
 
+ulbs AS (
+    SELECT
+        _id,
+        name,
+        state
+    FROM {{ source('cityfinance_prod', 'ulbs') }}
+),
 
 states AS (
-    SELECT *
+    SELECT
+        _id,
+        name
     FROM {{ source('cityfinance_prod', 'states') }}
-),
+)
+
+SELECT
+    --afs.*,
+
+    u.name AS ulb_name,
+    s.name AS state_name
+
+FROM afs
+LEFT JOIN ulbs u
+    ON afs.ulb = u._id
+
+LEFT JOIN states s
+    ON u.state = s._id
